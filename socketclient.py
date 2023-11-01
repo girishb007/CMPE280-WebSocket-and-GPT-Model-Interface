7d95671c737617b8dce34ea710775f9d16d1c016
Girish Bisane <girishbisane9096@gmail.com>
2023-10-31 20:32:54 -0700

    Push

--- socketclient.py
+++ socketclient.py
@@ -13,30 +13,30 @@ class WebSocketClient:
         master.title("CMPE 280 Assignment - Web Sockets and GPT Models")
 
         self.style = ttk.Style()
-        self.style.configure('TLabel', font=('Arial', 14), background='#d0e0f0')
-        self.style.configure('TButton', font=('Arial', 12), relief=tk.FLAT)
-        self.style.configure('TFrame', background='#d0e0f0')
-        self.style.configure('TText', font=('Arial', 12), padding=5, background='#d0e0f0', borderwidth=2, relief="solid")
+        self.style.configure('TLabel', font=('Arial', 16), background='#b2fab4', foreground='black')
+        self.style.configure('TButton', font=('Arial', 14), relief=tk.FLAT, background='#8cd9a0', foreground='black')
+        self.style.configure('TFrame', background='#b2fab4')
+        self.style.configure('TText', font=('Arial', 12), padding=5, background='#c8f0cc', foreground='black', borderwidth=2, relief="solid")
 
-        self.master.configure(background='#d0e0f0')
+        self.master.configure(background='#b2fab4')
 
-        self.label = ttk.Label(master, text="CMPE 280 - WebSocket Client Interface")
-        self.label.pack(pady=10)
+        self.label = ttk.Label(master, text="WebSocket Client Interface")
+        self.label.pack(pady=20)
 
         self.start_button = ttk.Button(master, text="Start", command=self.start, style='TButton')
-        self.start_button.pack(pady=5)
+        self.start_button.pack(pady=10, padx=20, side=tk.LEFT, expand=True)
 
         self.stop_button = ttk.Button(master, text="Stop", command=self.stop, state=tk.DISABLED, style='TButton')
-        self.stop_button.pack(pady=5)
+        self.stop_button.pack(pady=10, padx=20, side=tk.RIGHT, expand=True)
 
         self.log_frame = ttk.Frame(master)
-        self.log_frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+        self.log_frame.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
 
         self.log_label = ttk.Label(self.log_frame, text="Response Logs")
         self.log_label.pack(pady=(0, 5))
 
-        self.log = tk.Text(self.log_frame, height=10, width=50, state=tk.DISABLED, wrap=tk.WORD)
-        self.log.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.log = tk.Text(self.log_frame, height=15, width=80, state=tk.DISABLED, wrap=tk.WORD)
+        self.log.pack(fill=tk.BOTH, expand=True)
 
         self.scrollbar = ttk.Scrollbar(self.log_frame, command=self.log.yview)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
@@ -64,7 +64,7 @@ class WebSocketClient:
             for i in range(1, 10001):
                 if not self.running:
                     break
-                await websocket.send("Request [" + str(i) + "] Hello world!")
+                await websocket.send("Request [" + str(i) + "] Response recived!")
                 message = await websocket.recv()
                 self.log['state'] = tk.NORMAL
                 self.log.insert(tk.END, f"Received: {message}\n")
@@ -77,4 +77,3 @@ class WebSocketClient:
 root = tk.Tk()
 client = WebSocketClient(root)
 root.mainloop()
-
